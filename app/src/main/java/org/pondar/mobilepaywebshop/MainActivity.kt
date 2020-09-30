46f398e3f85b7c3a16ffe7f69453cd6494385589
zaifrun <martinknud@gmail.com>
2020-09-30 11:48:45 +0200

    Initial commit

---
+++ app/src/main/java/org/pondar/mobilepaywebshop/MainActivity.kt
@@ -0,0 +1,81 @@
+package org.pondar.mobilepaywebshop
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import dk.mobilepay.sdk.Country
+import dk.mobilepay.sdk.MobilePay
+import dk.mobilepay.sdk.ResultCallback
+import dk.mobilepay.sdk.model.FailureResult
+import dk.mobilepay.sdk.model.Payment
+import dk.mobilepay.sdk.model.SuccessResult
+import kotlinx.android.synthetic.main.activity_main.*
+import java.math.BigDecimal
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val MOBILEPAY_PAYMENT_REQUEST_CODE = 1337
+    private var isMobilePayInstalled = MobilePay.getInstance().isMobilePayInstalled(applicationContext)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Using mobile pay test merchant code!
+
+        MobilePay.getInstance().init("APPDK0000000000", Country.DENMARK)
+
+        buy_button.setOnClickListener { placeOrder(50.0,"my_order_id_string") }
+
+
+
+    }
+
+
+    private fun placeOrder(price: Double, orderId: String) {
+
+        if (isMobilePayInstalled) {
+            // MobilePay is present on the system. Create a Payment object.
+            val payment = Payment()
+            payment.productPrice = BigDecimal(price)
+            payment.orderId = orderId
+
+            // Create a payment Intent using the Payment object from above.
+            val paymentIntent = MobilePay.getInstance().createPaymentIntent(payment)
+
+            // We now jump to MobilePay to complete the transaction. Start MobilePay and wait for the result using an unique result code of your choice.
+            startActivityForResult(paymentIntent, MOBILEPAY_PAYMENT_REQUEST_CODE)
+        } else {
+            // MobilePay is not installed. Use the SDK to create an Intent to take the user to Google Play and download MobilePay.
+            val intent = MobilePay.getInstance().createDownloadMobilePayIntent(applicationContext)
+            startActivity(intent)
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == MOBILEPAY_PAYMENT_REQUEST_CODE) {
+            // The request code matches our MobilePay Intent
+            MobilePay.getInstance().handleResult(resultCode, data, object : ResultCallback {
+                override fun onSuccess(result: SuccessResult?) {
+                    // The payment succeeded - you can deliver the product.
+                    Log.d("IntentResult","Success")
+                }
+
+                override fun onFailure(result: FailureResult?) {
+                    Log.d("IntentResult","failure: ${result?.errorMessage}")
+
+                    // The payment failed - show an appropriate error message to the user. Consult the MobilePay class documentation for possible error codes.
+                }
+
+                override fun onCancel(orderId: String?) {
+                    // The payment was cancelled.
+                    Log.d("IntentResult","Buy was cancelled by user")
+                }
+            })
+        }
+    }
+}
